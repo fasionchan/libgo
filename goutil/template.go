f7366425a24323aa862d424b0d4ff0cd0b49d038
Fasion Chan <fasionchan@gmail.com>
2021-10-13 08:47:15 +0800

    add template func map

---
+++ goutil/template.go
@@ -0,0 +1,47 @@
+/*
+ * Author: fasion
+ * Created time: 2021-10-12 19:21:32
+ * Last Modified by: fasion
+ * Last Modified time: 2021-10-12 19:47:21
+ */
+
+package goutil
+
+import (
+	"reflect"
+	"text/template"
+)
+
+type TemplateFuncMap template.FuncMap
+
+func (m TemplateFuncMap) Native() template.FuncMap {
+	return template.FuncMap(m)
+}
+
+func (m TemplateFuncMap) Dup() TemplateFuncMap {
+	result := TemplateFuncMap{}
+	for k, v := range m {
+		result[k] = v
+	}
+	return result
+}
+
+func (m TemplateFuncMap) With(key string, f interface{}) TemplateFuncMap {
+	m[key] = f
+	return m
+}
+
+var TemplateHelpers = TemplateFuncMap{
+	"limit": func(data interface{}, n int) interface{} {
+		if n < 0 {
+			n = 0
+		}
+
+		value := reflect.ValueOf(data)
+		if value.Len() <= n {
+			return data
+		}
+
+		return value.Slice(0, n)
+	},
+}

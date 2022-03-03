dfc6f6c4caace507c5dc32caeefa6a76094c68c3
Fasion Chan <fasionchan@gmail.com>
2022-03-03 17:07:59 +0800

    template helpers

--- goutil/template.go
+++ goutil/template.go
@@ -2,7 +2,7 @@
  * Author: fasion
  * Created time: 2021-10-12 19:21:32
  * Last Modified by: fasion
- * Last Modified time: 2022-02-09 09:11:51
+ * Last Modified time: 2022-03-02 08:37:22
  */
 
 package goutil
@@ -46,6 +46,9 @@ var TemplateHelpers = TemplateFuncMap{
 
 		return value.Slice(0, n).Interface()
 	},
+	"now":   time.Now,
+	"today": Today,
+	"dayOf": DayOf,
 	"timefmt": func(t time.Time, fmt string, zeroPlaceHolder string) string {
 		if t.IsZero() {
 			return zeroPlaceHolder

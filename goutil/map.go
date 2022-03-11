64a896db2cf66dcd8dfdfe8b8b8a328a9ebb8b72
Fasion Chan <fasionchan@gmail.com>
2022-03-11 11:48:20 +0800

    ITYWPTJS-443: filter map

--- goutil/map.go
+++ goutil/map.go
@@ -2,7 +2,7 @@
  * Author: fasion
  * Created time: 2022-03-10 13:59:02
  * Last Modified by: fasion
- * Last Modified time: 2022-03-10 15:17:01
+ * Last Modified time: 2022-03-11 08:46:58
  */
 
 package goutil
@@ -11,24 +11,70 @@ import (
 	"strings"
 )
 
-type DataMappingByPath map[string]interface{}
+type DataMappingByKey map[string]interface{}
 
-func (m DataMappingByPath) GetByPrefix(prefix string) (result DataSlice) {
-	for path, data := range m {
-		if strings.HasPrefix(path, prefix) {
-			result = append(result, data)
-		}
-	}
-	return
+func (m DataMappingByKey) Len() int {
+	return len(m)
 }
 
-type DatasMappingByPath map[string]DataSlice
+func (m DataMappingByKey) Empty() bool {
+	return m.Len() == 0
+}
 
-func (m DatasMappingByPath) GetByPrefix(prefix string) (result DataSlice) {
-	for path, datas := range m {
-		if strings.HasPrefix(path, prefix) {
-			result = append(result, datas...)
+func (m DataMappingByKey) Keys() StringSlice {
+	keys := make(StringSlice, 0, len(m))
+	for key, _ := range m {
+		keys = keys.Append(key)
+	}
+	return keys
+}
+
+func (m DataMappingByKey) Values() DataSlice {
+	values := make(DataSlice, 0, len(m))
+	for _, value := range m {
+		values = values.Append(value)
+	}
+	return values
+}
+
+func (m DataMappingByKey) Datas() DataSlice {
+	return m.Values()
+}
+
+func (m DataMappingByKey) FilterByAnyPrefix(prefixes StringSlice) DataMappingByKey {
+	result := DataMappingByKey{}
+	for key, data := range m {
+		if prefixes.AnyMatch(func(s string) bool {
+			return strings.HasPrefix(key, s)
+		}) {
+			result[key] = data
 		}
 	}
-	return
+	return result
+}
+
+func (m DataMappingByKey) FilterByPrefix(prefixes ...string) DataMappingByKey {
+	return m.FilterByAnyPrefix(prefixes)
+}
+
+type DatasMappingByKey map[string]DataSlice
+
+func (m DatasMappingByKey) Keys() StringSlice {
+	keys := make(StringSlice, 0, len(m))
+	for key, _ := range m {
+		keys = keys.Append(key)
+	}
+	return keys
+}
+
+func (m DatasMappingByKey) FilterByAnyPrefix(prefixes StringSlice) DatasMappingByKey {
+	result := DatasMappingByKey{}
+	for key, datas := range m {
+		if prefixes.AnyMatch(func(s string) bool {
+			return strings.HasPrefix(key, s)
+		}) {
+			result[key] = datas
+		}
+	}
+	return result
 }
